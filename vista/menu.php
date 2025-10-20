787dc5fa970614222d0fd0566d251947ee7c767d
Diegosolerfo <diegosolerfo90@gmail.com>
2025-10-19 19:13:20 -0500

    docs: agregando estilos

---
+++ vista/menu.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="productos.php"><img src="../imagenes/caballo_piñata.png" class="imagen" alt="imagen"></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="productos.php">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="tiposdeproductos.php">Tipos de productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../index.html">Cerrar sesión</a>
+            </li>
+        </ul>
+    </div>
+</div>
