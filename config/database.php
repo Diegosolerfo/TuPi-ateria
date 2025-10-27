812d697d49d7a781d28c9ce8eb51389081c88efd
Diegosolerfo <diegosolerfo90@gmail.com>
2025-10-26 19:50:32 -0500

    docs:escaneo 3

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'host' => 'localhost',
+    'dbname' => 'proyecto_pi',
+    'username' => 'root',
+    'password' => '', // Dejar vacío o poner la contraseña que uses localmente
+    'charset' => 'utf8mb4',
+    'options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ]
+];
